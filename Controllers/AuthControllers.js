27cb4831b531e8fcdf90ac141c3fdc72f29d16d0
ronie-coder <ronweasly9435@gmail.com>
2025-07-15 00:29:16 +0530

    middleware fix

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -1,76 +1,80 @@
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const UserModel = require("../Models/User")
-const jwt_creator = require("../Helpers/TokenCreator")
-class AuthControllers{
-  async register_new_user(req,res){
-    const{username,email,password} = req.body
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../Models/User");
+const jwt_creator = require("../Helpers/TokenCreator");
+class AuthControllers {
+  async register_new_user(req, res) {
+    const { username, email, password } = req.body;
     try {
-      const UserExists = await UserModel.findOne({$or:[{username:username},{email:email}]})
-      if(UserExists){
+      const UserExists = await UserModel.findOne({
+        $or: [{ username: username }, { email: email }],
+      });
+      if (UserExists) {
         return res.status(404).json({
-          success:false,
-          message:"The user already exists"
-        })
+          success: false,
+          message: "The user already exists",
+        });
       }
-      const hashed_pass = bcrypt.hashSync(password,10)
+      const hashed_pass = bcrypt.hashSync(password, 10);
       const new_user = await UserModel.create({
-        username,email,password:hashed_pass
-      })
+        username,
+        email,
+        password: hashed_pass,
+      });
       return res.status(200).json({
-        success:true,
-        message:"User created successfully",
-        dets:new_user
-      })
+        success: true,
+        message: "User created successfully",
+        dets: new_user,
+      });
     } catch (error) {
       return res.status(500).json({
-        success:false,
-        error:error
-      })
+        success: false,
+        error: error,
+      });
     }
   }
-  async login_user(req,res){
-    const{email,password} = req.body
+  async login_user(req, res) {
+    const { email, password } = req.body;
     try {
-      const UserExists = await UserModel.findOne({email:email})
-      if(!UserExists){
+      const UserExists = await UserModel.findOne({ email: email });
+      if (!UserExists) {
         return res.status(404).json({
-          success:false,
-          message:"Invalid credentials"
-        })
+          success: false,
+          message: "Invalid credentials",
+        });
       }
-      const pass_valid = bcrypt.compareSync(password,UserExists.password)
-      if(!pass_valid){
+      const pass_valid = bcrypt.compareSync(password, UserExists.password);
+      if (!pass_valid) {
         return res.status(404).json({
-          success:false,
-          message:"Invalid credentials"
-        })
+          success: false,
+          message: "Invalid credentials",
+        });
       }
-      const payload_details = {username:UserExists.username,email:UserExists.email}
-      const access_token = jwt_creator(payload_details,"1m")
-      const refresh_token = jwt_creator(payload_details,"2m")
-      res.cookie("access_token",access_token,{
+      const payload_details = {
+        username: UserExists.username,
+        email: UserExists.email,
+      };
+      const access_token = jwt_creator(payload_details, "1m");
+      const refresh_token = jwt_creator(payload_details, "2m");
+      res.cookie("access_token", access_token, {
         maxAge: 1 * 60 * 1000, // 1 minute
         httpOnly: true, // for security
         secure: false, // set to true in production (HTTPS)
-        sameSite: "strict", // CSRF protection  
-      })
-      res.cookie("refresh_token",refresh_token,{
+        sameSite: "strict", // CSRF protection
+      });
+      res.cookie("refresh_token", refresh_token, {
         maxAge: 2 * 60 * 1000, // 1 minute
         httpOnly: true, // for security
         secure: false, // set to true in production (HTTPS)
-        sameSite: "strict", // CSRF protection  
-      })
-
+        sameSite: "strict", // CSRF protection
+      });
 
       return res.status(200).json({
-        success:true,
-        message:"LoggedIn Successfully"
-      })
-    } catch (error) {
-      
-    }
+        success: true,
+        message: "LoggedIn Successfully",
+      });
+    } catch (error) {}
   }
 }
 
-module.exports = new AuthControllers()
+module.exports = new AuthControllers();
--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -1,68 +1,72 @@
-const jwt = require("jsonwebtoken")
-const UserModel = require("../Models/User")
-class AuthMiddlewares{
-  async checkAuthorized(req,res,next){
-    const access_token = req.cookies?.access_token
-    const refresh_token = req.cookies?.refresh_token
+const jwt = require("jsonwebtoken");
+const UserModel = require("../Models/User");
+class AuthMiddlewares {
+  async checkAuthorized(req, res, next) {
+    const access_token = req.cookies?.access_token;
+    const refresh_token = req.cookies?.refresh_token;
     try {
-      if(!access_token && !refresh_token){
+      if (!access_token && !refresh_token) {
         return res.status(404).json({
-          success:false,
-          message:"Not Authorized"
-        })
+          success: false,
+          message: "Not Authorized",
+        });
       }
       try {
-        
-        const access_token_valid = jwt.verify(access_token,process.env.JWT_SECRET)
-        req.userDetails = access_token_valid
-        return next()
+        const access_token_valid = jwt.verify(
+          access_token,
+          process.env.JWT_SECRET
+        );
+        req.userDetails = access_token_valid;
+        return next();
       } catch (error) {
-        if(!refresh_token){
+        if (!refresh_token) {
           return res.status(404).json({
-          success:false,
-          message:"No refresh token foundd" //strictly for debugging
-        })
+            success: false,
+            message: "No refresh token foundd", //strictly for debugging
+          });
         }
       }
-      const refresh_token_valid = jwt.verify(refresh_token,process.env.JWT_SECRET)
+      const refresh_token_valid = jwt.verify(
+        refresh_token,
+        process.env.JWT_SECRET
+      );
       const new_access_token = {
-        username:refresh_token_valid.username,
-        email:refresh_token_valid.email
-      }
-      res.cookie("access_token",new_access_token,{
+        username: refresh_token_valid.username,
+        email: refresh_token_valid.email,
+      };
+      res.cookie("access_token", new_access_token, {
         maxAge: 1 * 60 * 1000, // 1 minute
         httpOnly: true, // for security
         secure: false, // set to true in production (HTTPS)
-        sameSite: "strict", // CSRF protection  
-      })
-      req.userDetails = refresh_token_valid
-      next()
+        sameSite: "strict", // CSRF protection
+      });
+      req.userDetails = refresh_token_valid;
+      next();
     } catch (error) {
       return res.status(500).json({
-        success:false,
-        error:error
-      })
+        success: false,
+        error: error,
+      });
     }
-
   }
-  async checkIfAdmin(req,res,next){
-    const userDetails = req.userDetails
+  async checkIfAdmin(req, res, next) {
+    const userDetails = req.userDetails;
     try {
-      const user = await UserModel.findOne({username:userDetails.username})
-      if(user.role != "admin"){
+      const user = await UserModel.findOne({ username: userDetails.username });
+      if (user.role != "admin") {
         return res.status(404).json({
-          success:false,
-          message:"You are not an admin"
-        })
+          success: false,
+          message: "You are not an admin",
+        });
       }
-      return next()
+      return next();
     } catch (error) {
       return res.status(500).json({
-        success:false,
-        error:error
-      })
+        success: false,
+        error: error,
+      });
     }
   }
 }
 
-module.exports = new AuthMiddlewares()
+module.exports = new AuthMiddlewares();
